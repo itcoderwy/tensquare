40d675d551f68fdb65b171cefb835be08315b215
superbugwww <718670035@qq.com>
2020-04-27 14:38:11 +0800

    新增标签服务   按条件查询和分页查询

--- tensquare_parent/tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
+++ tensquare_parent/tensquare_base/src/main/java/com/tensquare/base/controller/LabelController.java
@@ -2,16 +2,21 @@ package com.tensquare.base.controller;
 
 import com.tensquare.base.pojo.Label;
 import com.tensquare.base.service.LabelService;
+import entity.PageResult;
 import entity.Result;
 import entity.StatusCode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @ClassName LabelController
- * @Description TODO
+ * @Description 标签
  * @Author lenovo
  * @Date 2020/4/26 22:10
  * @Version 1.0
@@ -48,6 +53,31 @@ public class LabelController {
         label.setId(id);
         labelService.update(label);
         return new Result(true, StatusCode.OK,"更新成功");
+    }
+
+    /**
+     * 根据条件查询
+     * @param map 查询条件
+     * @return 所有符合条件数据
+     */
+    @PostMapping("/search")
+    public Result findSearch(@RequestBody Map map) {
+        List<Label> list = labelService.findSearch(map);
+        return new Result(true, StatusCode.OK, "查询成功", list);
+    }
+
+    /**
+     * 分页条件查询
+     * @param map 查询条件
+     * @param page 当前页 注意页数从0开始 0代表第一页
+     * @param size 每页显示记录数
+     */
+    @PostMapping("/search/{page}/{size}")
+    public Result findSearch(@RequestBody(required = false) Map map, @PathVariable int page, @PathVariable int size) {
+        Pageable pageable = PageRequest.of(page-1, size);
+        Page<Label> pageData = labelService.findSearch(map, pageable);
+        PageResult<Label> pageResult = new PageResult<>(pageData.getTotalElements(), pageData.getContent());
+        return new Result(true, StatusCode.OK, "查询成功", pageResult);
     }
 
 
--- tensquare_parent/tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
+++ tensquare_parent/tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
@@ -3,10 +3,20 @@ package com.tensquare.base.service;
 import com.tensquare.base.dao.LabelDao;
 import com.tensquare.base.pojo.Label;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import utils.IdWorker;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @ClassName LabelService
@@ -45,8 +55,59 @@ public class LabelService {
         return  labelDao.findAll();
     }
 
+    /**
+     * 构建查询条件对象（查询返回符合条件数据，分页条件方法都可用）
+     *
+     * @param map 标签查询条件
+     */
+    public Specification<Label> createSpecification(Map map){
+       return new Specification<Label>() {
+           @Override
+           public Predicate toPredicate(Root<Label> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+               List<Predicate> list = new ArrayList<Predicate>();
+               if(map!=null && !map.isEmpty()){
+                   if (!StringUtils.isEmpty(map.get("labelname"))) {
+                       Predicate p1 = criteriaBuilder.like(root.get("labelname").as(String.class), "%"+(String) map.get("labelname")+"%");
+                       list.add(p1);
+                   }
+                   if (!StringUtils.isEmpty(map.get("state"))) {
+                       Predicate p1 = criteriaBuilder.equal(root.get("state").as(String.class), (String) map.get("state"));
+                       list.add(p1);
+                   }
+                   if (!StringUtils.isEmpty(map.get("recommend"))) {
+                       Predicate p1 = criteriaBuilder.equal(root.get("recommend").as(String.class), (String) map.get("recommend"));
+                       list.add(p1);
+                   }
+                   if(list.size()==0){
+                       return null;
+                   }
+                   return criteriaBuilder.and(list.toArray(new Predicate[list.size()]));
+               }
+               return null;
+           }
+       };
+    }
 
+    /**
+     * 标签条件查询（无分页）
+     * @param map
+     * @return
+     */
+    public List<Label> findSearch(Map map){
+        Specification<Label> specification = createSpecification(map);
+        return labelDao.findAll(specification);
+    }
 
+    /**
+     *分页条件查询
+     * @param map
+     * @param pageable
+     * @return
+     */
+    public Page<Label> findSearch(Map map, Pageable pageable){
+        Specification<Label> specification = createSpecification(map);
+        return labelDao.findAll(specification, pageable);
+    }
 
 
 }
